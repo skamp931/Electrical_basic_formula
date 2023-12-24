c4cdc57cf940ac4431b5de6c0037d5b1784171b8
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-24 22:12:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,14 @@ if add_radio =="電気工学_基本":
         st.write('電流I（アンペア）を求める')
         orm_1 = st.number_input("抵抗R_")
         volt_1 = st.number_input("電圧V_")
-        ampea_1 = volt_1 * orm_1 
+        ampea_1 = volt_1 / orm_1 
         st.write(f"　電流Iは{ampea_1:,.3f}Vです")
 
     with col3:
         st.write('抵抗R（オーム）を求める')
         volt_1 = st.number_input("電圧V__")
         ampea_1 = st.number_input("電流I__")
-        orm_1 = volt_1 * ampea_1 
+        orm_1 = volt_1 / ampea_1 
         st.write(f"　抵抗Rは{orm_1:,.3f}Vです")
 
 
